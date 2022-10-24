f6ffa22bf05c1488b5d2116cbb7267fa085ae408
Mankopodi Kgopotso <malomamankopodikgopotso@Mankopodis-MacBook-Air.local>
2022-10-24 18:13:19 +0200

    monday

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './Cpmponent/about/about.component';
+import { ContactComponent } from './Cpmponent/contact/contact.component';
+import { HomeComponent } from './Cpmponent/home/home.component';
+import { NavbarComponent } from './Cpmponent/navbar/navbar.component';
+import { PortfolioComponent } from './Cpmponent/portfolio/portfolio.component';
+import { ResumeComponent } from './Cpmponent/resume/resume.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'navbar' ,component: NavbarComponent},
+  {path: 'home' ,component: HomeComponent},
+  {path: 'about' ,component: AboutComponent},
+  {path: 'resume' ,component: ResumeComponent},
+  {path: 'portfolio' ,component: PortfolioComponent},
+  {path: 'contact' ,component: ContactComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
